a98e40ce522e5d9cf4ff1685f85b3742c5707e2e
andrey rogov <andrew0rohov@gmail.com>
2025-08-12 15:47:47 +0300

    ToDo: add startUI.h setup

--- core/client.cpp
+++ core/client.cpp
@@ -1,65 +1,59 @@
 #include <winsock2.h>
 #include <iostream>
 #include <cstring>
+#include "client.h"
+
 #pragma comment(lib, "Ws2_32.lib")
 
-class Client {
-public:
-    bool Connect() {
-        WSADATA wsaData;
-        if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0) {
-            std::cout << "WSAStartup failed!" << std::endl;
-            return false;
-        }
+bool Client::Connect() { 
+    WSADATA wsaData;
+    if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0) {
+        std::cout << "WSAStartup error!" << std::endl;
+        return false;
+    }
+    SOCKET sock = socket(AF_INET, SOCK_STREAM, 0);
+    if (sock == INVALID_SOCKET) {
+        std::cout << "Socket Conn error!" << std::endl;
+        WSACleanup();
+        return false;
+    }
+    sockaddr_in serverAddr;
 
-        SOCKET sock = socket(AF_INET, SOCK_STREAM, 0);
-        if (sock == INVALID_SOCKET) {
-            std::cout << "Socket creation failed!" << std::endl;
-            WSACleanup();
-            return false;
-        }
+    serverAddr.sin_family = AF_INET;
+    serverAddr.sin_addr.s_addr = inet_addr("127.0.0.1");
+    serverAddr.sin_port = htons(1111);
 
-        sockaddr_in serverAddr;
-        serverAddr.sin_family = AF_INET;
-        serverAddr.sin_addr.s_addr = inet_addr("127.0.0.1");
-        serverAddr.sin_port = htons(1111); 
 
-        std::cout << "Connecting to server..." << std::endl;
-        if (connect(sock, (SOCKADDR*)&serverAddr, sizeof(serverAddr)) == SOCKET_ERROR) {
-            std::cout << "Connect failed!" << std::endl;
-            closesocket(sock);
-            WSACleanup();
-            return false;
-        }
-
-        std::cout << "Connected to server!" << std::endl;
-
-        
-        const char* message = "Hello from client!";
-        if (send(sock, message, strlen(message), 0) == SOCKET_ERROR) {
-            std::cout << "Send failed!" << std::endl;
-        } else {
-            std::cout << "Message sent!" << std::endl;
-        }
-
-        
-        char buffer[256];
-        int bytes = recv(sock, buffer, sizeof(buffer) - 1, 0);
-        if (bytes > 0) {
-            buffer[bytes] = '\0';
-            std::cout << "Reply from server: " << buffer << std::endl;
-        }
-
-        
+    std::cout << "Conn to server..." << std::endl;
+    if (connect(sock, (SOCKADDR*)&serverAddr, sizeof(serverAddr)) == SOCKET_ERROR) {
+        std::cout << "" << std::endl;
         closesocket(sock);
         WSACleanup();
-        return true;
+        return false;
     }
-};
+    std::cout << "Connected to server!" << std::endl;
+    const char* message = "Hello from client!";
+
+    if (send(sock, message, strlen(message), 0) == SOCKET_ERROR) {
+        std::cout << "Message failed!" << std::endl;
+    } else {
+        std::cout << "Message sent!" << std::endl;
+    }
+
+    char buffer[256];
+    int bytes = recv(sock, buffer, sizeof(buffer) - 1, 0);
+    if (bytes > 0) {
+        buffer[bytes] = '\0';
+        std::cout << "Reply from server: " << buffer << std::endl;
+    }
+
+    closesocket(sock);
+    WSACleanup();
+    return true;
+}
 
 int main() {
     Client client;
     client.Connect();
-
     return 0;
 }
---
+++ core/client.h
@@ -0,0 +1,10 @@
+#pragma once
+
+#include <winsock2.h>
+#include <iostream>
+#include <cstring>
+
+class Client {
+public:
+    bool Connect();
+};
