ca965eb21d1000bd940cc4f6f8f6d2640fb4537a
andrey rogov <@a.d.rogov>
2025-08-15 08:25:22 +0300

    add inputField

--- UI/clientUI.cpp
+++ UI/clientUI.cpp
@@ -4,10 +4,14 @@
 class MainFrame : public wxFrame {
 public:
     MainFrame() : wxFrame(nullptr, wxID_ANY, "CppWinsock", wxDefaultPosition, wxSize(800, 60)) {
-        // auto* sizer = new wxBoxSizer(wxHORIZONTAL);
+        auto* sizer = new wxBoxSizer(wxHORIZONTAL);
+
+        inputField = new wxTextCtrl(this, wxID_ANY, "textLabel", wxDefaultPosition, wxDefaultSize, wxTE_PROCESS_ENTER);
+        sizer->Add(inputField, 0, wxCENTER | wxALL, 10);
         
     }
-
+private:
+    wxTextCtrl* inputField;
 
 
 };
